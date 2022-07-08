84875396564e41468e6d2bd192effc5f1cc57558
hillary-cloud <obicj97@gmail.com>
2022-07-08 07:02:56 -0700

    dissabled registration

--- routes/auth.php
+++ routes/auth.php
@@ -11,15 +11,18 @@ use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
-    Route::get('register', [RegisteredUserController::class, 'create'])
-                ->name('register');
+    // This dissabled the registration 
 
-    Route::post('register', [RegisteredUserController::class, 'store']);
+    // Route::get('register', [RegisteredUserController::class, 'create'])
+    //             ->name('register');
 
-    Route::get('login', [AuthenticatedSessionController::class, 'create'])
+    // Route::post('register', [RegisteredUserController::class, 'store']);
+
+    // added admin/ before of login
+    Route::get('admin/login', [AuthenticatedSessionController::class, 'create'])
                 ->name('login');
 
-    Route::post('login', [AuthenticatedSessionController::class, 'store']);
+    Route::post('admin/login', [AuthenticatedSessionController::class, 'store'])->name('login');
 
     Route::get('forgot-password', [PasswordResetLinkController::class, 'create'])
                 ->name('password.request');
