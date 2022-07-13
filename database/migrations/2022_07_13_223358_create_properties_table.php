39466f46cbb1442241b364b7aabbac961dbbb197
hillary-cloud <obicj97@gmail.com>
2022-07-13 16:10:04 -0700

    created image model and migration

--- database/migrations/2022_07_13_223358_create_properties_table.php
+++ database/migrations/2022_07_13_223358_create_properties_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\Models\Location;
 
 return new class extends Migration
 {
@@ -15,6 +16,24 @@ return new class extends Migration
     {
         Schema::create('properties', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->string('description');
+            $table->foreignIdFor(Location::class);
+            $table->string('address');
+            $table->string('price');
+            $table->foreignIdFor(Category::class);
+            $table->string('property_type');
+            $table->string('bed_room')->nullable();
+            $table->string('bath_room')->nullable();
+            $table->string('kitchen')->nullable();
+            $table->enum('status', ['instock','outofstock']);
+            $table->string('landlord_name')->nullable();
+            $table->string('landlord_phone')->nullable();
+            $table->string('agent_name')->nullable();
+            $table->string('agent_phone')->nullable();
+            $table->string('main_image')->nullable();
+
             $table->timestamps();
         });
     }
